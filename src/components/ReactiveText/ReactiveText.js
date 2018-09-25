dbd0791d225c2d7086f13d70836ca52249a8b810
danielpruitt <35474097+danielpruitt@users.noreply.github.com>
2018-09-25 18:14:43 -0400

    trying to deploy

--- src/components/ReactiveText/ReactiveText.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ReactiveText = (props) => (
-    <div> 
-        <h4 handleImageClick={props.handleImageClick}>{props.message}</h4>
-    </div>
-);
-
-export default ReactiveText;
